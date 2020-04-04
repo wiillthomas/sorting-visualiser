ea4e55166da4c10d631759f718e5407de4bce0d7
Will Thomas <will.thomas@roardigital.co.uk>
2020-04-04 09:15:12 +0100

    GH pages commit

--- src/algos/index.js
+++
@@ -1,9 +0,0 @@
-import bubbleSort from "./bubbleSort"
-import quickSort from "./quickSort"
-import insertionSort from "./insertionSort"
-
-export {
-    bubbleSort,
-    quickSort,
-    insertionSort
-}
